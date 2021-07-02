17e4bc4e0d211921847f00bb015135b79bf2d633
Adriano <adriano.faria@outlook.com.br>
2021-07-02 16:45:24 -0300

    Clock

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import HelloWorld from './HelloWorld';
+import Clock from './Clock';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <HelloWorld/>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Clock />
       </header>
     </div>
   );
