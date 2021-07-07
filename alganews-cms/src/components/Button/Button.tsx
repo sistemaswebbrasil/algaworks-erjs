a8dd8a53b0fe935610e71b795b217d436c65002c
Adriano <adriano.faria@outlook.com.br>
2021-07-07 08:24:01 -0300

    https://app.algaworks.com/aulas/3730/criando-as-variantes-do-botao?pagina=0

--- alganews-cms/src/components/Button/Button.tsx
+++ alganews-cms/src/components/Button/Button.tsx
@@ -1,11 +1,12 @@
 import * as B from './Button.styles'
 
 export interface ButtonProps {
-
+  variant: 'danger' | 'text' | 'primary'
+  label: string
 }
 
-export default function Button ({ }: ButtonProps) {
-    return <B.Wrapper>        
-        Botão        
-    </B.Wrapper>
+export default function Button ({ label, variant }: ButtonProps) {
+  return <B.Wrapper variant={variant}>
+    { label }
+  </B.Wrapper>
 }
