8eebcd4c21c579d115d6ad807a842598b80c370d
Adriano <adriano.faria@outlook.com.br>
2021-07-19 11:47:20 -0300

    https://app.algaworks.com/aulas/3763/desafio-progress-bar

---
+++ alganews-cms/src/stories/ProgressBar.stories.tsx
@@ -0,0 +1,49 @@
+import { Story, Meta } from '@storybook/react';
+import ProgressBar, { ProgressBarProps } from '../components/ProgressBar/ProgressBar';
+
+export default {
+  title: 'Example/ProgressBar',
+  component: ProgressBar,
+} as Meta;
+
+const Template: Story<ProgressBarProps> = (args) => <ProgressBar {...args} />;
+
+export const Primary = Template.bind({})
+Primary.args = {
+  title: 'Aguarde',
+  progress: 50,
+  theme:'primary',  
+  width:375
+}
+
+export const Secondary = Template.bind({})
+Secondary.args = {
+  title: 'Aguarde',
+  progress: 60,
+  theme:'secondary',
+  width:375
+}
+
+export const Complete = Template.bind({})
+Complete.args = {
+  title: 'Aguarde',
+  progress: 100,
+  theme:'primary',
+  width:375
+}
+
+export const ZeroProgress = Template.bind({})
+ZeroProgress.args = {
+  title: 'Aguarde',
+  progress: 0,
+  theme:'primary',
+  width:375
+}
+
+export const ProgressInHalfOfText = Template.bind({})
+ProgressInHalfOfText.args = {
+  title: 'Aguarde',
+  progress: 10,
+  theme:'primary',
+  width:375
+}
