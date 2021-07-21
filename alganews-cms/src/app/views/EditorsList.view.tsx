aad42e16e95cfdb7f85d736ab3be632a03eacc19
Adriano <adriano.faria@outlook.com.br>
2021-07-21 08:47:03 -0300

    https://app.algaworks.com/aulas/3804/feature-editorslist

--- alganews-cms/src/app/views/EditorsList.view.tsx
+++ alganews-cms/src/app/views/EditorsList.view.tsx
@@ -1,10 +1,15 @@
 import usePageTitle from '../../core/hooks/usePageTitle'
+import EditorsList from '../features/EditorsList'
 import DefaultLayout from '../layouts/Default'
 
 function EditorsListView() {
   usePageTitle('Lista de Editores')
 
-  return <DefaultLayout>todo</DefaultLayout>
+  return (
+    <DefaultLayout>
+      <EditorsList />
+    </DefaultLayout>
+  )
 }
 
 export default EditorsListView
