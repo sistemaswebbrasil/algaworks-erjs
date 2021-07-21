f25f33fbbb69e4ff6b365fca5bf46ce36de3e5f2
Adriano <adriano.faria@outlook.com.br>
2021-07-21 07:41:55 -0300

    https://app.algaworks.com/aulas/3801/feature-userearnings

--- alganews-cms/src/app/views/Home.view.tsx
+++ alganews-cms/src/app/views/Home.view.tsx
@@ -1,14 +1,19 @@
 import usePageTitle from "../../core/hooks/usePageTitle"
 import PostList from "../features/PostsList"
+import UserEarnings from "../features/UserEarnings"
 import UserPerformance from "../features/UserPerformance"
+import UserToptags from "../features/UserToptags"
 import DefaultLayout from "../layouts/Default"
-import UserToptags from "./UserToptags"
+
 
 export default function Home () {
   usePageTitle('Home')
 
   return <DefaultLayout>
-    <UserToptags />
+    <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr', alignItems: 'center', gap: 32 }}>
+      <UserToptags />
+      <UserEarnings />
+    </div>
     <UserPerformance />
     <PostList />
   </DefaultLayout>
