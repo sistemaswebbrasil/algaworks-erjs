070597e667a70cdbd0b3674d90ecf015a5b0a684
Adriano Faria Alves <adriano.faria@outlook.com.br>
2021-09-18 19:23:18 -0300

    https://app.algaworks.com/aulas/3863/fazendo-o-loading-funcionar

--- alganews-cms/src/app/features/PostForm.tsx
+++ alganews-cms/src/app/features/PostForm.tsx
@@ -7,6 +7,7 @@ import PostService from "../../sdk/services/Post.service"
 import Button from "../components/Button/Button"
 import ImageUpload from "../components/ImageUpload"
 import Input from "../components/Input/Input"
+import Loading from "../components/Loading"
 import MarkdownEditor from "../components/MarkdownEditor"
 import TagInput from "../components/TagInput"
 import WordPriceCounter from "../components/WordPriceCounter"
@@ -17,24 +18,33 @@ export default function PostForm () {
   const [title, setTitle] = useState('')
   const [imageUrl, setImageUrl] = useState('')
 
+  const [publishing, setPublishing] = useState(false)
+
   async function handleFormSubmit (e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
-    const newPost = {
-      body,
-      title,
-      tags: tags.map(tag => tag.text),
-      imageUrl,
+    try {
+      setPublishing(true)
+      const newPost = {
+        body,
+        title,
+        tags: tags.map(tag => tag.text),
+        imageUrl,
+      }
+
+      const insertedPost = await PostService
+        .insertNewPost(newPost)
+
+      info({
+        title: 'Post salvo com sucesso',
+        description: 'Você acabou de criar o post com o id ' + insertedPost.id
+      })
+    } finally {
+      setPublishing(false)
     }
-
-    const insertedPost = await PostService.insertNewPost(newPost)
-
-    info({
-      title: 'Post salvo com sucesso',
-      description: 'Você acabou de criar o post com o id ' + insertedPost.id
-    })
   }
 
   return <PostFormWrapper onSubmit={handleFormSubmit}>
+    <Loading show={publishing} />
     <Input
       label="título"
       value={title}
--- alganews-cms/src/app/views/Home.view.tsx
+++ alganews-cms/src/app/views/Home.view.tsx
@@ -1,6 +1,5 @@
 import usePageTitle from "../../core/hooks/usePageTitle"
 import ErrorBoundary from "../components/ErrorBoundary"
-import Loading from "../components/Loading"
 import PostList from "../features/PostsList"
 import UserEarnings from "../features/UserEarnings"
 import UserPerformance from "../features/UserPerformance"
@@ -11,7 +10,6 @@ export default function Home() {
   usePageTitle('Home')
 
   return <DefaultLayout>
-    <Loading />
     <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr', alignItems: 'center', gap: 32 }}>
       <ErrorBoundary component={'top tags'}>
         <UserTopTags />
