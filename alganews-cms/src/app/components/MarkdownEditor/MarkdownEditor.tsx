5014b142c6193617b1a287400393a3a125a9fae6
Adriano Faria Alves <adriano.faria@outlook.com.br>
2021-09-18 20:59:23 -0300

    https://app.algaworks.com/aulas/3876/upload-de-imagem-no-post

--- alganews-cms/src/app/components/MarkdownEditor/MarkdownEditor.tsx
+++ alganews-cms/src/app/components/MarkdownEditor/MarkdownEditor.tsx
@@ -1,6 +1,7 @@
 import MarkdownIt from 'markdown-it'
 import MdEditor, { Plugins } from 'react-markdown-editor-lite'
 import 'react-markdown-editor-lite/lib/index.css';
+import FileService from '../../../sdk/services/File.service';
 
 MdEditor.unuse(Plugins.FontUnderline)
 
@@ -28,8 +29,12 @@ export interface MarkdownEditorProps {
 }
 
 export default function MarkdownEditor (props: MarkdownEditorProps) {
+  async function handleImageUpload(file: File) {
+    return FileService.upload(file)
+  }
   return <MdEditor
     readOnly={props.readOnly}
+    onImageUpload={handleImageUpload}
     style={{ height: props.readOnly ? 'auto' : 300 }}
     value={props.value}
     renderHTML={text => parser.render(text)}
