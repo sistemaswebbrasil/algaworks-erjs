46a9675f5b68d278e454ca0c57e0cce3999b6fc3
Adriano Faria Alves <adriano.faria@outlook.com.br>
2021-09-19 10:17:33 -0300

    https://app.algaworks.com/aulas/3888/a-importancia-do-prepublish

--- alganews-sdk/src/index.ts
+++ alganews-sdk/src/index.ts
@@ -1,3 +1,7 @@
 export default function log () {
-    console.log('uhu, i am a log function')
-}
+    console.log('uhu, I am a log function')
+  }
+  
+  export function feature () {
+    console.log('hi I am a feature') 
+  }
